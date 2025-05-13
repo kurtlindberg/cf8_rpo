29056f137e25c6c969e3cbeb0017cf9545677108
Kurt R. Lindberg <85883737+kurtlindberg@users.noreply.github.com>
2025-05-12 20:21:25 -0400

    Update header and comments

--- cf8_rpo_figures/cf8rpo_fig4_icy.py
+++ cf8_rpo_figures/cf8rpo_fig4_icy.py
@@ -1,16 +1,18 @@
-## Lake CF8 RPO Figure 4
-# Inverse Cumulative Yield plots of RPO CO2
+## Postglacial carbon cycling history of a northeastern Baffin Island lake catchment inferred from ramped pyrolysis oxidation and radiocarbon dating
 
-# Postglacial carbon cycling history of a northeastern Baffin Island lake catchment inferred from ramped pyrolysis oxidation and radiocarbon dating
+## Manuscript authors: Kurt R. Lindberg, Elizabeth K. Thomas, Brad E. Rosenheim, Gifford H. Miller, Julio Sepulveda, Devon R. Firesinger,
+## Gregory A. de Wet, Benjamin V. Gaglioti
 
-# Manuscript authors: Kurt R. Lindberg, Elizabeth K. Thomas, Brad E. Rosenheim, Gifford H. Miller, Julio Sepulveda, Devon R. Firesinger,
-# Gregory A. de Wet, Benjamin V. Gaglioti
+## DOI: pending
 
-# DOI: pending
+## Code Author: Kurt R. Lindberg
 
-# Author: Kurt R. Lindberg
-# Last edited: 09/25/2024
+### Figure 4 ###
+## (a) Inverse cummulative yield plot
+## (b) Inverse cummulative yield y-intercept vs bulk 14C
 
+
+## See cf8_rpo_conda_env.yml
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -21,93 +23,114 @@ import pyleoclim as pyleo
 from pylipd.lipd import LiPD
 import cf8rpo_functions as cf8_fun
 
+
+## Figure parameters for editing in Inkscape
 plt.rcParams['svg.fonttype'] = 'none'
-plt.rcParams['font.size'] = 12
-plt.rcParams['font.family'] = "sans-serif"
+plt.rcParams['font.size'] = 10
+plt.rcParams['font.family'] = "Liberation Sans"
+
 
 ## Import data from LiPD files
-s1_conc_ens, s1_conc = cf8_fun.getlipd('Lindberg.CF8.2024.lpd',
-                               paleoData_variableName='split1concentration',
-                               depth_name='RPOdepth',
-                               val_unit='umol')
+s1_conc_ens, s1_conc = cf8_fun.getlipd(
+  'Lindberg.CF8.2024.lpd',
+  paleoData_variableName='split1concentration',
+  depth_name='RPOdepth',
+  val_unit='umol'
+)
+s2_conc_ens, s2_conc = cf8_fun.getlipd(
+  'Lindberg.CF8.2024.lpd',
+  paleoData_variableName='split2concentration',
+  depth_name='RPOdepth',
+  val_unit='umol'
+)
+s3_conc_ens, s3_conc = cf8_fun.getlipd(
+  'Lindberg.CF8.2024.lpd',
+  paleoData_variableName='split3concentration',
+  depth_name='RPOdepth',
+  val_unit='umol'
+)
+s4_conc_ens, s4_conc = cf8_fun.getlipd(
+  'Lindberg.CF8.2024.lpd',
+  paleoData_variableName='split4concentration',
+  depth_name='RPOdepth',
+  val_unit='umol'
+)
+s5_conc_ens, s5_conc = cf8_fun.getlipd(
+  'Lindberg.CF8.2024.lpd',
+  paleoData_variableName='split5concentration',
+  depth_name='RPOdepth',
+  val_unit='umol'
+)
 
-s2_conc_ens, s2_conc = cf8_fun.getlipd('Lindberg.CF8.2024.lpd',
-                               paleoData_variableName='split2concentration',
-                               depth_name='RPOdepth',
-                               val_unit='umol')
+s1_fm_ens, s1_fm = cf8_fun.getlipd(
+  'Lindberg.CF8.2024.lpd',
+  paleoData_variableName='split1fractionModern',
+  depth_name='RPOdepth',
+  val_unit ='unitless'
+)
+s2_fm_ens, s2_fm = cf8_fun.getlipd(
+  'Lindberg.CF8.2024.lpd',
+  paleoData_variableName='split2fractionModern',
+  depth_name='RPOdepth',
+  val_unit ='unitless'
+)
+s3_fm_ens, s3_fm = cf8_fun.getlipd(
+  'Lindberg.CF8.2024.lpd',
+  paleoData_variableName='split3fractionModern',
+  depth_name='RPOdepth',
+  val_unit ='unitless'
+)
+s4_fm_ens, s4_fm = cf8_fun.getlipd(
+  'Lindberg.CF8.2024.lpd',
+  paleoData_variableName='split4fractionModern',
+  depth_name='RPOdepth',
+  val_unit ='unitless'
+)
+s5_fm_ens, s5_fm = cf8_fun.getlipd(
+  'Lindberg.CF8.2024.lpd',
+  paleoData_variableName='split5fractionModern',
+  depth_name='RPOdepth',
+  val_unit ='unitless'
+)
 
-s3_conc_ens, s3_conc = cf8_fun.getlipd('Lindberg.CF8.2024.lpd',
-                               paleoData_variableName='split3concentration',
-                               depth_name='RPOdepth',
-                               val_unit='umol')
-
-s4_conc_ens, s4_conc = cf8_fun.getlipd('Lindberg.CF8.2024.lpd',
-                               paleoData_variableName='split4concentration',
-                               depth_name='RPOdepth',
-                               val_unit='umol')
-
-s5_conc_ens, s5_conc = cf8_fun.getlipd('Lindberg.CF8.2024.lpd',
-                               paleoData_variableName='split5concentration',
-                               depth_name='RPOdepth',
-                               val_unit='umol')
-
-s1_fm_ens, s1_fm = cf8_fun.getlipd('Lindberg.CF8.2024.lpd',
-                           paleoData_variableName='split1fractionModern',
-                           depth_name='RPOdepth',
-                           val_unit ='unitless')
-
-s2_fm_ens, s2_fm = cf8_fun.getlipd('Lindberg.CF8.2024.lpd',
-                           paleoData_variableName='split2fractionModern',
-                           depth_name='RPOdepth',
-                           val_unit ='unitless')
-
-s3_fm_ens, s3_fm = cf8_fun.getlipd('Lindberg.CF8.2024.lpd',
-                           paleoData_variableName='split3fractionModern',
-                           depth_name='RPOdepth',
-                           val_unit ='unitless')
-
-s4_fm_ens, s4_fm = cf8_fun.getlipd('Lindberg.CF8.2024.lpd',
-                           paleoData_variableName='split4fractionModern',
-                           depth_name='RPOdepth',
-                           val_unit ='unitless')
-
-s5_fm_ens, s5_fm = cf8_fun.getlipd('Lindberg.CF8.2024.lpd',
-                           paleoData_variableName='split5fractionModern',
-                           depth_name='RPOdepth',
-                           val_unit ='unitless')
-
-rpo_depth_ens, rpo_depth = cf8_fun.getlipd('Lindberg.CF8.2024.lpd',
-                               paleoData_variableName='RPOdepth',
-                               depth_name='RPOdepth',
-                               val_unit='cm')
+rpo_depth_ens, rpo_depth = cf8_fun.getlipd(
+  'Lindberg.CF8.2024.lpd',
+  paleoData_variableName='RPOdepth',
+  depth_name='RPOdepth',
+  val_unit='cm'
+)
 
 
 ## Calculations for CO2 amount-weighted bulk RPO, inverse CO2 cummulative yield
-
-# CF817-03 macrofossil fraction modern values reported in Crump et al. (2021)
-# DOI: https://doi.org/10.1073/pnas.2019069118
+## CF817-03 macrofossil fraction modern values reported in Crump et al. (2021)
+## DOI: https://doi.org/10.1073/pnas.2019069118
 cf8_noaa_chron = pd.read_excel("Lindberg_2024_CF8_NOAA_paleoclimate.xlsm",
                          sheet_name="Chronology",
                          header=19)
 macro_fm = np.array(cf8_noaa_chron.fraction_modern)
 
-co2_df = pd.concat([pd.Series(data=s1_conc.paleoData_values, name='s1'),
-                    pd.Series(data=s2_conc.paleoData_values, name='s2'),
-                    pd.Series(data=s3_conc.paleoData_values, name='s3'),
-                    pd.Series(data=s4_conc.paleoData_values, name='s4'),
-                    pd.Series(data=s5_conc.paleoData_values, name='s5')],
-                    axis=1)
-
-fm_df = pd.concat([pd.Series(data=s1_fm.paleoData_values, name='s1'),
-                   pd.Series(data=s2_fm.paleoData_values, name='s2'),
-                   pd.Series(data=s3_fm.paleoData_values, name='s3'),
-                   pd.Series(data=s4_fm.paleoData_values, name='s4'),
-                   pd.Series(data=s5_fm.paleoData_values, name='s5')],
-                   axis=1)
-
+co2_df = pd.concat(
+  [
+    pd.Series(data=s1_conc.paleoData_values, name='s1'),
+    pd.Series(data=s2_conc.paleoData_values, name='s2'),
+    pd.Series(data=s3_conc.paleoData_values, name='s3'),
+    pd.Series(data=s4_conc.paleoData_values, name='s4'),
+    pd.Series(data=s5_conc.paleoData_values, name='s5')
+  ], axis=1
+)
+fm_df = pd.concat(
+  [
+    pd.Series(data=s1_fm.paleoData_values, name='s1'),
+    pd.Series(data=s2_fm.paleoData_values, name='s2'),
+    pd.Series(data=s3_fm.paleoData_values, name='s3'),
+    pd.Series(data=s4_fm.paleoData_values, name='s4'),
+    pd.Series(data=s5_fm.paleoData_values, name='s5')
+  ], axis=1
+)
 co2_yield = np.array(co2_df)
 fm_arr = np.array(fm_df)
 
+## Convert RPO CO2 split concentrations to inverse cummulative yield
 icy = np.zeros(np.shape(np.array(co2_yield)))
 for i in range(0, np.shape(icy)[0]):
     for j in range(0, np.shape(icy)[1]):
@@ -116,20 +139,23 @@ for i in range(0, np.shape(icy)[0]):
 co2_frac = closure(co2_yield)
 icy_yint = np.zeros(np.shape(icy)[0])
 
+## Calculate RPO CO2 concentration-weighted average Fm
 fm_bulk = np.zeros(np.shape(icy)[0])
 for i in range(0, np.shape(fm_bulk)[0]):
     fm_bulk[i] = np.sum(co2_frac[i,:]*fm_arr[i,:])/np.sum(co2_frac[i,:])
 
 
-## Figure Script
-fig5_colors = ['#00441b',
-               '#1b7837',
-               '#5aae61',
-               '#a6dba0',
-               '#c2a5cf',
-               '#9970ab',
-               '#762a83',
-               '#40004b',]
+## Figure 4 a-b script
+fig5_colors = [
+  '#00441b',
+  '#1b7837',
+  '#5aae61',
+  '#a6dba0',
+  '#c2a5cf',
+  '#9970ab',
+  '#762a83',
+  '#40004b'
+]
 
 fig, axs = plt.subplots(2, 2)
 
@@ -172,3 +198,8 @@ print(bulk_corr)
 
 figure4 = plt.gcf()
 # figure4.savefig('cf8rpo_figure4.svg')
+
+
+## Figure 4 a-b script
+
+
