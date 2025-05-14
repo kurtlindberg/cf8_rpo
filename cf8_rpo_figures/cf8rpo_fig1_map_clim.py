f90c5f1be68e6d5daab52f97a7413b47bbd9adb4
Kurt R. Lindberg <85883737+kurtlindberg@users.noreply.github.com>
2025-05-14 12:18:51 -0400

    Update formatting

--- cf8_rpo_figures/cf8rpo_fig1_map_clim.py
+++ cf8_rpo_figures/cf8rpo_fig1_map_clim.py
@@ -31,7 +31,7 @@ plt.rcParams['font.family'] = "Liberation Sans"
 
 
 ## Define cartopy features
-projLcc = ccrs.LambertConformal(central_longitude=-70,central_latitude=60)
+projLcc = ccrs.LambertConformal(central_longitude=-70, central_latitude=60)
 resol = '50m'
 land = cfeature.NaturalEarthFeature('physical', 'land', scale=resol, edgecolor='black')
 border = cfeature.NaturalEarthFeature(category='cultural', name='admin_0_boundary_lines_land', scale=resol, facecolor='none')
@@ -46,7 +46,7 @@ ax.add_feature(land, facecolor='white', alpha=0.7, zorder=5)
 ax.add_feature(border, alpha=0.7, linewidth=1, zorder=50)
 ax.add_feature(ocean, zorder=1)
 # ax.add_feature(lakes, zorder=25)
-ax.set_extent([-85, -55, 59, 76], crs=ccrs.PlateCarree())
+ax.set_extent([-85,-55,59,76], crs=ccrs.PlateCarree())
 
 plt.plot([-68.94968], [70.55818], '*', color='red', markeredgecolor='black', markersize=20, zorder=100, transform=ccrs.PlateCarree())
 
@@ -133,27 +133,27 @@ precip_plot.insert(
 
 
 ## Figure 1b script
-fig, axs = plt.subplots(2, 1)
+fig, axs = plt.subplots(2,1)
 
-# ax = axs[0, 0]
+# ax = axs[0,0]
 ax = axs[0]
 sns.pointplot(ax=ax, x=temp_plot.Month, y=temp_plot.Temp, color='red', marker=None, linewidth=1.5)
 ax.set_xticklabels("")
 ax.set_xlabel("")
 ax.set_ylabel("Temp. (C)")
-ax.set_yticks(ticks=[-30, -20, -10, 0, 10])
-ax.set_ylim([-35, 10])
+ax.set_yticks(ticks=[-30,-20,-10,0,10])
+ax.set_ylim([-35,10])
 
-# ax = axs[1, 0]
+# ax = axs[1,0]
 ax = axs[1]
 sns.pointplot(ax=ax, x=precip_plot.Month, y=(precip_plot.Precip*1000), color='blue', marker=None, linewidth=1.5)
 ax.set_xlabel("")
 ax.set_ylabel("Precip. (mm)")
-ax.set_yticks(ticks=[0, 20, 40, 60])
-ax.set_ylim([0, 70])
+ax.set_yticks(ticks=[0,20,40,60])
+ax.set_ylim([0,70])
 
-# fig.delaxes(axs[0, 1])
-# fig.delaxes(axs[1, 1])
+# fig.delaxes(axs[0,1])
+# fig.delaxes(axs[1,1])
 
 fig1b = plt.gcf()
 # fig1b.savefig('cf8rpo_fig1b_clim.svg')
